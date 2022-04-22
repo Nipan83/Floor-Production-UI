b01023d0e543c59e368682f0a04b406ec62134a2
Nipan <nipandas83@gmail.com>
2022-04-23 01:14:04 +0530

    updates on station list

---
+++ src/components/Admin/StationList/Station.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from "react";
+import {Button, Row, Col} from 'react-bootstrap';
+import './Station.css';
+
+const Station = () => {
+
+    return (
+        <div>
+            Station Details
+        </div>
+    )
+}
+
+export default Station;
