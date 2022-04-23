7af19fef0acb64769d0d8c2d579760bf2fc7fed2
Nipan <nipandas83@gmail.com>
2022-04-24 02:34:16 +0530

    WIP Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,28 @@
 import React, {useState, useEffect} from "react";
 import {Button, Row, Col} from 'react-bootstrap';
+import HourlyDashboard from './HourlyDashboard/HourlyDashboard.js'
+import Station from './StationList/Station.js'
+import './Dashboard.css'
 
 const Dashboard = () => {
 
     return (
-        <div className = "container">
-            Dashboard
+        <div>
+            <Row className="justify-content-around">
+                <Col sm={4} className="panel left-panel">
+                    <div>
+                        <Station></Station>
+                    </div>
+                </Col>
+                <Col sm={4} className="panel mid-panel">
+                    <div>MID</div>
+                </Col> 
+                <Col sm={4} className="panel right-panel">
+                    <div>
+                        <HourlyDashboard></HourlyDashboard>
+                    </div>
+                </Col>  
+            </Row>
         </div>
     )
 }
